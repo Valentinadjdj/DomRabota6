3f611aa74317938e6a12f5dc3e6cc141ee0495f0
Valentina Korotkova <korobok.valek@mail.ru>
2023-06-11 00:51:03 +0600

    Второй вариант решения первой задачи

--- Program.cs
+++ Program.cs
@@ -30,3 +30,57 @@ double x = (-b2 + b1)/(-k1 + k2);
 double y = k2 * x + b2;
 
 Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+
+//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через запятую: ");
+int[] numb = StringToNum(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < numb.Length; i++)
+{
+    if (numb[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
