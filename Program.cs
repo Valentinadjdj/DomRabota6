bf087c7300be73c7f4699c0a10fd947aa72f8daa
Valentina Korotkova <korobok.valek@mail.ru>
2023-06-11 00:26:02 +0600

    Решение первой задачи

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы(через пробел): ");
+double[] arr = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
