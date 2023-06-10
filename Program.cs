dda206ffee5c17479256b18491efb945b4a4c47e
Valentina Korotkova <korobok.valek@mail.ru>
2023-06-11 00:41:39 +0600

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите элементы(через пробел): ");
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите элементы(через пробел): ");
 double[] arr = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
 int count = 0;
  
@@ -11,3 +13,20 @@ for (int i = 0; i < arr.Length; i++)
 }
  
 Console.WriteLine($"Кол-во элементов больше нуля: {count}");
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
